a94ba779e70b0538627b6444a2af27b0016c6b35
Tahlil <bsse0803@iit.du.ac.bd>
2022-09-16 23:15:11 +0600

    Add function to instantiate new projects

--- contracts/Protocol.sol
+++ contracts/Protocol.sol
@@ -50,4 +50,26 @@ contract Protocol is AccessControl {
         require(isRegulator(msg.sender), 'Not a regulator');
         nftContract.mintNewProject(account, data, pollutantDetails);
     }
+
+    function instantiateNewProjects(
+        uint256 nftID,
+        address account,
+        uint256 amount,
+        bytes memory data,
+        INFT.Pollutant[] memory pollutantDetails,
+        bytes32 _hashedMessage1,
+        uint8 _v1,
+        bytes32 _r1,
+        bytes32 _s1,
+        bytes32 _hashedMessage2,
+        uint8 _v2,
+        bytes32 _r2,
+        bytes32 _s2
+    ) public {
+        address signer1 = VerifySignature(_hashedMessage1, _v1, _r1, _s1);
+        address signer2 = VerifySignature(_hashedMessage2, _v2, _r2, _s2);
+        require(isRegulator(signer1), 'Not a regulator');
+        require(isOffsetter(signer2), 'Not a offsetter');
+        nftContract.mintProjects(nftID, account, amount, data, pollutantDetails);
+    }
 }
