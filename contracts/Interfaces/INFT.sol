203cc01ad3c4a53d92d54ac63ef1445ddf26d5a6
Tahlil <bsse0803@iit.du.ac.bd>
2022-09-16 22:45:23 +0600

    Add nft interface

---
+++ contracts/Interfaces/INFT.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.15;
+
+interface INFT {
+    struct Pollutant {
+        string name;
+        uint256[] intialAmounts;
+        uint256[] targetAmounts;
+    }
+
+    function mintNewProject(
+        address account,
+        bytes memory data,
+        Pollutant[] memory pollutantDetails
+    ) external;
+
+    function mintProjects(
+        address account,
+        uint256 amount,
+        bytes memory data,
+        Pollutant[] memory pollutantDetails
+    ) external;
+}
