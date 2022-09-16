8256deadfb9dc97d810e43a1a780fc49665fec3d
Tahlil <bsse0803@iit.du.ac.bd>
2022-09-16 23:11:01 +0600

    Add new project from protocol contract

--- contracts/Protocol.sol
+++ contracts/Protocol.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 import '@openzeppelin/contracts/access/AccessControl.sol';
+import './interfaces/INFT.sol';
 
 contract Protocol is AccessControl {
     bytes32 public constant REGULATORY_AUTHORITY_ROLE = keccak256('REGULATORY_AUTHORITY_ROLE');
     bytes32 public constant OFFSETTER_ROLE = keccak256('OFFSETTER_ROLE');
 
     bool intializeCalled;
-    address nftAddress;
+    INFT nftContract;
 
     constructor(address reguulatorAddress, address offsetrerAddress) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
@@ -17,7 +18,7 @@ contract Protocol is AccessControl {
 
     function initialize(address _nftAddress) public {
         require(!intializeCalled, 'Already intialize');
-        nftAddress = _nftAddress;
+        nftContract = INFT(_nftAddress);
         intializeCalled = true;
     }
 
@@ -39,5 +40,14 @@ contract Protocol is AccessControl {
 
     function isOffsetter(address addressToCheck) public view returns (bool) {
         return hasRole(OFFSETTER_ROLE, addressToCheck);
+    }
+
+    function addNewProject(
+        address account,
+        bytes memory data,
+        INFT.Pollutant[] memory pollutantDetails
+    ) public {
+        require(isRegulator(msg.sender), 'Not a regulator');
+        nftContract.mintNewProject(account, data, pollutantDetails);
     }
 }
