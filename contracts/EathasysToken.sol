0eb0d42d4454f72a7f096adb2fd283a932ef01eb
Tahlil <bsse0803@iit.du.ac.bd>
2022-09-16 19:05:52 +0600

    Remove burnable function

--- contracts/EathasysToken.sol
+++ contracts/EathasysToken.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.15;
 import '@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol';
-import '@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155BurnableUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155SupplyUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
 import '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
@@ -14,7 +13,6 @@ contract Earthasys is
     ERC1155Upgradeable,
     AccessControlUpgradeable,
     PausableUpgradeable,
-    ERC1155BurnableUpgradeable,
     ERC1155SupplyUpgradeable,
     UUPSUpgradeable
 {
@@ -32,7 +30,6 @@ contract Earthasys is
         __ERC1155_init('https://bafybeicw3mj2lc3k6fc2zs5g4zravbh2ekddkhadbjj7elngtqebkh6xyu.ipfs.nftstorage.link/');
         __AccessControl_init();
         __Pausable_init();
-        __ERC1155Burnable_init();
         __ERC1155Supply_init();
         __UUPSUpgradeable_init();
 
