41e890f0d40d958912968fd055aaad65926d1029
Tahlil <bsse0803@iit.du.ac.bd>
2022-09-18 18:46:41 +0600

    Deploy all contracts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,6 +5,8 @@ const path = require('path');
 const { readFileSync } = require('fs');
 
 async function main() {
+  console.log(__dirname);
+  
   const Protocol = await ethers.getContractFactory('Protocol');
   const testnetExplorerLink = 'https://testnet.snowtrace.io/address/';
 
@@ -18,12 +20,16 @@ async function main() {
   const EarthasysNFT = await ethers.getContractFactory('EarthasysNFT');
   const nft = await EarthasysNFT.deploy(protocol.address);
   await nft.deployed();
-
   console.log('Protocol contract deployed to: ', protocol.address);
   console.log('Earthasys NFT contract deployed to: ', nft.address);
   console.log('Protocol contract link: ', testnetExplorerLink + protocol.address);
   console.log('Earthasys NFT contract link: ', testnetExplorerLink + nft.address);
 
+  const ERC20Addresses = {
+    "Carbon": "0x4c9F85aC612507E93E6B94106E90b083A87E2495",
+    "Lead": "0x3D3736Cb1f5aC304CDb2FAB2db6e70c629E382Aa",
+    "Dust": "0x9b9cd543DC1586c053e566F16390DCc7A76902Fd"
+  };
   const erc20s = JSON.parse(readFileSync(path.join(__dirname, 'data.json').toString()));
   for (const erc20 of erc20s) {
     // eslint-disable-next-line no-await-in-loop
@@ -38,12 +44,13 @@ async function main() {
     // eslint-disable-next-line no-await-in-loop
     await tx.wait();
     // eslint-disable-next-line prefer-template, no-await-in-loop
-    console.log(erc20.PollutantName + ' ERC20 of address' + (await nft.getERC20Address(erc20.PollutantName)));
+    console.log(erc20.PollutantName + ' ERC20 of address ' + (await nft.getERC20Address(erc20.PollutantName)));
+    ERC20Addresses[erc20.PollutantName] = erc20.PollutantName;
   }
 
   await writeFile(
-    './src/info/data.json',
-    JSON.stringify({ protocolAddress: protocol.address, nftAddress: nft.address }),
+    path.join(__dirname, '../', 'frontend', 'src', 'info', 'data.json'),
+    JSON.stringify({ protocolAddress: protocol.address, nftAddress: nft.address, ERC20Addresses: ERC20Addresses }, null, 2),
   );
 }
 
